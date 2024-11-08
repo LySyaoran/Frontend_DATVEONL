3103380bc05f1ab36491cdfb34c78f327dd09c1d
LySyaoran <ngao57257@gmail.com>
2024-11-08 22:54:00 +0700

    Change firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, RecaptchaVerifier , signInWithPhoneNumber } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDnZgMQbSKnZDpF3edU9nJmn6tekI0OaXo",
-  authDomain: "smsverify-d618b.firebaseapp.com",
-  projectId: "smsverify-d618b",
-  storageBucket: "smsverify-d618b.firebasestorage.app",
-  messagingSenderId: "814482131899",
-  appId: "1:814482131899:web:95f2d0975560932fc8929e",
-  measurementId: "G-Q2R8K4QW1J"
+  apiKey: "AIzaSyBONtQosDUT30uK5Csne-TJCAExDpJge1U",
+  authDomain: "smsverify-8bbff.firebaseapp.com",
+  projectId: "smsverify-8bbff",
+  storageBucket: "smsverify-8bbff.firebasestorage.app",
+  messagingSenderId: "410134379702",
+  appId: "1:410134379702:web:cbc3e2ed1e9f53d213ba17",
+  measurementId: "G-FCV95ZNCPG"
 };
 
 // Initialize Firebase
